b9e5c5f70392c54d24032236d3d48f3e907fd657
ninjaoflight <jonguz5@hotmail.com>
2020-03-26 15:37:17 -0600

    implementado cliente

--- backend/src/banco/backend/estructuras/Cliente.java
+++ backend/src/banco/backend/estructuras/Cliente.java
@@ -10,5 +10,51 @@ package banco.backend.estructuras;
  * @author jonathan
  */
 public class Cliente {
-    
+
+    public Cliente(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public Cliente(int cedula, String nombre, String apellidos, String numero) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.numero = numero;
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public Cliente setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public Cliente setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+        return this;
+    }
+
+    public Cliente setNumero(String numero) {
+        this.numero = numero;
+        return this;
+    }
+
+    public int cedula;
+    public String nombre = "";
+    public String apellidos = "";
+    public String numero = "";
 }
