ed04eaad54a41843d7d74503be9c5b9bc5d01e43
ninjaoflight5 <jonguz5@hotmail.com>
2020-03-28 18:42:16 -0600

    implementado agregarCliente()

--- backend/src/main/java/banco/backend/dao/BancoDAO.java
+++ backend/src/main/java/banco/backend/dao/BancoDAO.java
@@ -6,6 +6,7 @@
 package banco.backend.dao;
 
 import banco.backend.estructuras.Cliente;
+import banco.backend.estructuras.Cuenta;
 import banco.backend.estructuras.Usuario;
 import java.io.IOException;
 import java.sql.Connection;
@@ -25,7 +26,7 @@ public class BancoDAO {
     //<editor-fold desc="Metodos de generales" defaultstate="collapsed">
     private BancoDAO() {
         try {
-            
+
             this.cfg.load(getClass().getResourceAsStream(UBICACION_CREDENCIALES));
             this.baseDatos = cfg.getProperty("base_datos");
             this.usuario = cfg.getProperty("usuario");
@@ -124,7 +125,53 @@ public class BancoDAO {
         return resultado;
     }
 
+    public boolean agregarCliente(Cliente cliente) {
+        try (Connection cnx = obtenerConexion();
+                PreparedStatement stm = cnx.prepareStatement(CMD_AGREGAR_CLIENTE)) {
+
+            stm.clearParameters();
+            stm.setInt(1, cliente.getCedula());
+            stm.setString(2, cliente.getNombre());
+            stm.setString(3, cliente.getApellidos());
+            stm.setString(4, cliente.getNumero());
+
+            return stm.executeUpdate() == 1;
+        } catch (Exception ex) {
+            System.err.printf("No se pudo insertar un cliente nuevo: %s \n", ex.getMessage());
+        }
+        return false;
+    }
+
     //</editor-fold>
+    //<editor-fold desc="Cuenta" defaultstate="collapsed">
+    public Cuenta recuperarCuenta(int cedula) {
+        Cuenta resultado = null;
+        try (Connection cnx = obtenerConexion();
+                PreparedStatement stm = cnx.prepareStatement(CMD_RECUPERAR_CLIENTE)) {
+            stm.clearParameters();
+
+            stm.setInt(1, cedula);
+
+            try (ResultSet rs = stm.executeQuery()) {
+                if (rs.next()) {
+                    resultado
+                            = new Cuenta(
+                                    rs.getInt(1),
+                                    rs.getInt(2),
+                                    rs.getString(3),
+                                    rs.getBigDecimal(4),
+                                    rs.getInt(4)
+                            );
+                }
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+            }
+        } catch (Exception ex) {
+            String error = ex.getLocalizedMessage();
+            System.err.println(error);
+        }
+        return resultado;
+    }
     //</editor-fold>
     private static BancoDAO instancia = null;
     private static final String UBICACION_CREDENCIALES = "/configuraciones/credenciales-bd.properties";
@@ -140,6 +187,8 @@ public class BancoDAO {
             = "select * from usuario "
             + "where cedula = ?";
 
+    private static final String CMD_AGREGAR_ADMIN
+            = "INSERT INTO administrador (cedula, pass) VALUES (?, ?);";
     private static final String CMD_RECUPERAR_ADMIN
             = "select * from administrador "
             + "where cedula = ?";
@@ -148,10 +197,12 @@ public class BancoDAO {
             = "UPDATE usuario "
             + "SET pass = ? "
             + "WHERE cedula=?, pass = ?;";
-
     private static final String CMD_RECUPERAR_CLIENTE
             = "select * from cliente "
             + "where cedula = ?";
+    private static final String CMD_AGREGAR_CLIENTE
+            = "insert into cliente (cedula, nombre, apellidos, telefono) "
+            + "values (?, ?, ?, ?)";
 
     //</editor-fold>
 }
