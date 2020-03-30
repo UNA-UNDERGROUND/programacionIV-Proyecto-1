6f88dfc603b0e5e9b23ff9fa76bfb522c18cf39f
ninjaoflight5 <jonguz5@hotmail.com>
2020-03-29 21:31:58 -0600

    usar DAO de moneda y cuenta

--- backend/src/main/java/banco/backend/Controlador.java
+++ backend/src/main/java/banco/backend/Controlador.java
@@ -7,9 +7,15 @@ package banco.backend;
 
 import banco.backend.db.BancoDAO;
 import banco.backend.estructuras.Cliente;
+import banco.backend.estructuras.Cuenta;
+import banco.backend.estructuras.Moneda;
 import banco.backend.estructuras.Usuario;
 import banco.backend.estructuras.dao.ClienteDAO;
+import banco.backend.estructuras.dao.CuentaDAO;
+import banco.backend.estructuras.dao.MonedaDAO;
 import banco.backend.estructuras.dao.UsuarioDAO;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -43,7 +49,52 @@ public class Controlador {
         return daoCliente.actualizarCliente(cliente);
     }
     
+    public Cuenta recuperarCuenta(int idCuenta) {
+        return daoCuenta.recuperarCuenta(idCuenta);
+    }
+
+    public Object[] recuperarCuentas(Cliente cliente) {
+        Cuenta[] cuentas 
+                = daoCuenta.recuperarCuentas(cliente.getCedula());
+        Map<String, Moneda> monedas = new HashMap<>();
+        for (Cuenta cuenta : cuentas) {
+            String codigo = cuenta.getMoneda();
+            if (!monedas.containsKey(codigo)) {
+                monedas.put(
+                        codigo,
+                        daoMoneda.recuperarMoneda(codigo)
+                );
+            }
+        }
+        return new Object[]{cuentas, monedas};
+    }
+
+    public Object[] recuperarCuentasVinculadas(Cliente cliente) {
+        Cuenta[] cuentas 
+                = daoCuenta.recuperarCuentasVinculadas(cliente.getCedula());
+        Map<String, Moneda> monedas = new HashMap<>();
+        for (Cuenta cuenta : cuentas) {
+            String codigo = cuenta.getMoneda();
+            if (!monedas.containsKey(codigo)) {
+                monedas.put(
+                        codigo,
+                        daoMoneda.recuperarMoneda(codigo)
+                );
+            }
+        }
+        return new Object[]{cuentas, monedas};
+    }
+
+    public boolean agregarCuenta(Cuenta cuenta) {
+        return daoCuenta.agregarCuenta(cuenta);
+    }
+    public boolean agregarCuentaVinculada(Cliente cliente, Cuenta cuenta){
+        return daoCuenta.agregarCuentaVinculada(cliente.getCedula(), cuenta.getIdCuenta());
+    }
     
+    public Moneda[] recuperarMonedas() {
+        return daoMoneda.recuperarMonedas();
+    }
     private static Controlador instancia;
 
     public static Controlador getInstancia() {
@@ -52,4 +103,6 @@ public class Controlador {
     BancoDAO bd = new BancoDAO();
     UsuarioDAO daoUsuario = new UsuarioDAO();
     ClienteDAO daoCliente = new ClienteDAO();
+    CuentaDAO daoCuenta = new CuentaDAO();
+    MonedaDAO daoMoneda = new MonedaDAO();
 }
