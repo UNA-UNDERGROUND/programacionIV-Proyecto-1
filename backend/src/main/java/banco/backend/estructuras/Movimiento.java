d855a97f93a9d67c9a74538739b634658d3e8f9c
ninjaoflight5 <jonguz5@hotmail.com>
2020-03-29 19:21:18 -0600

    agregada clase movimiento

---
+++ backend/src/main/java/banco/backend/estructuras/Movimiento.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco.backend.estructuras;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+/**
+ *
+ * @author jonguz
+ */
+public class Movimiento {
+
+    Movimiento(
+            int idTransaccion,
+            int idCuenta,
+            boolean deposito,
+            BigDecimal monto,
+            String descripcion,
+            Date fechaDeposito)
+    {
+        this.idTransaccion=idTransaccion;
+        this.idCuenta=idCuenta;
+        this.deposito=deposito;
+        this.monto = monto;
+        this.descripcion=descripcion;
+        this.fechaDeposito = fechaDeposito;
+        
+    }
+    Movimiento(
+            int idCuenta,
+            boolean deposito,
+            BigDecimal monto,
+            String descripcion)
+    {
+        this.idCuenta=idCuenta;
+        this.deposito=deposito;
+        this.monto = monto;
+        this.descripcion=descripcion;
+    }
+    
+    public int getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public int getIdCuenta() {
+        return idCuenta;
+    }
+
+    public boolean esDeposito() {
+        return deposito;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Date getFechaDeposito() {
+        return fechaDeposito;
+    }
+
+    private int idTransaccion=0;
+    private int idCuenta;
+    private boolean deposito;
+    private BigDecimal monto;
+    private String descripcion="";
+    private Date fechaDeposito=null; 
+
+}
