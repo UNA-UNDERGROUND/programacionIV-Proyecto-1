aee786e152123765d90112675cc79b3572231b1e
ninjaoflight5 <jonguz5@hotmail.com>
2020-03-29 19:22:49 -0600

    implementacion de moneda

---
+++ backend/src/main/java/banco/backend/estructuras/Moneda.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco.backend.estructuras;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author jonguz
+ */
+public class Moneda {
+
+    public Moneda(
+            String codigo,
+            String nombre,
+            String signo,
+            BigDecimal precioCompra,
+            float porcentajeInteres) {
+        
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.signo = signo;
+            this.precioCompra = precioCompra;
+            this.porcentajeInteres = porcentajeInteres;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getSigno() {
+        return signo;
+    }
+
+    public BigDecimal getPrecioCompra() {
+        return precioCompra;
+    }
+
+    public float getPorcentajeInteres() {
+        return porcentajeInteres;
+    }
+
+    private final String codigo;
+    private final String nombre;
+    private final String signo;
+    private final BigDecimal precioCompra;
+    private final float porcentajeInteres;
+
+}
