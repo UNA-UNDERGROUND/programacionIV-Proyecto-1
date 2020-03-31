f29da52eb6ae82c7081146b3da7ccb0b885922c0
ninjaoflight5 <jonguz5@hotmail.com>
2020-03-31 17:44:18 -0600

    corregido nivel de acceso de contructores

--- backend/src/main/java/banco/backend/estructuras/Movimiento.java
+++ backend/src/main/java/banco/backend/estructuras/Movimiento.java
@@ -14,7 +14,7 @@ import java.sql.Date;
  */
 public class Movimiento {
 
-    Movimiento(
+    public Movimiento(
             int idTransaccion,
             int idCuenta,
             boolean deposito,
@@ -30,7 +30,7 @@ public class Movimiento {
 
     }
 
-    Movimiento(
+    public Movimiento(
             int idCuenta,
             boolean deposito,
             BigDecimal monto,
