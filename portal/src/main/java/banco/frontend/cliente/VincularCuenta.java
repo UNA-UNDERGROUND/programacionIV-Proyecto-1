f6ccf3e61ede580a021fcd37a84799659ef5f152
ninjaoflight5 <jonguz5@hotmail.com>
2020-04-24 22:27:22 -0600

    vincular cuentas

---
+++ portal/src/main/java/banco/frontend/cliente/VincularCuenta.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco.frontend.cliente;
+
+import banco.backend.Controlador;
+import banco.backend.estructuras.Cliente;
+import banco.backend.estructuras.Cuenta;
+import banco.backend.estructuras.Usuario;
+import banco.backend.estructuras.Movimiento;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jonguz
+ */
+@WebServlet(name = "ClientDashboardController", urlPatterns = {"/cliente/cuentas"})
+public class VisorCuentas extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String viewUrl = "";
+        if (validarSesion(request)) {
+            switch (request.getServletPath()) {
+                case "/cliente/cuentas":
+                    if(request.getParameter("cuenta")==null){
+                        viewUrl = verCuentas(request);
+                    }
+                    else{
+                        viewUrl =  verCuenta(request);
+                    }
+                    
+                    
+                    break;
+            }
+        }
+        else{
+            viewUrl = "/logout";
+        }
+
+        request.getRequestDispatcher(viewUrl).forward(request, response);
+    }
+    
+    public String verCuenta(HttpServletRequest request){
+        Integer idCuenta;
+        
+        try{
+            idCuenta = Integer.parseInt(request.getParameter("cuenta"));
+        }
+        catch(NumberFormatException ex){
+            idCuenta = null;
+        }
+        
+        if(idCuenta == null){
+            return "/presentation/Error.jsp";
+        }
+        else{
+            Controlador c = Controlador.getInstancia();
+            Usuario u = (Usuario) request.getSession().getAttribute("usuario");
+            Cliente cliente = c.recuperarDatosPersonales(u.getCedula());
+            
+            List<Movimiento> m 
+                    = Arrays.asList(
+                            c.recuperarMovimientos(idCuenta)
+                    );
+            request.setAttribute("movimientos", m);
+            request.setAttribute("cliente", cliente);
+        }
+        
+        return "/presentation/cliente/verCuentas.jsp";
+    }
+    
+    public String verCuentas(HttpServletRequest request){
+        Controlador c = Controlador.getInstancia();
+        Usuario u = (Usuario) request.getSession().getAttribute("usuario");
+        Cliente cliente = c.recuperarDatosPersonales(u.getCedula());
+        Object res[] = c.recuperarCuentas(cliente);
+        
+        List<Cuenta> cuenta = Arrays.asList((Cuenta[])res[0]);
+        request.setAttribute("cuentas", cuenta);
+
+        res = c.recuperarCuentasVinculadas(cliente);
+        List<Cuenta> cuentaV = Arrays.asList((Cuenta[])res[0]);
+        request.setAttribute("cuentasV", cuentaV);
+
+        
+        request.setAttribute("cliente", cliente);
+        return "/presentation/cliente/verCuentas.jsp";
+    }
+
+    public boolean validarSesion(HttpServletRequest request) {
+        Usuario u = (Usuario) request.getSession().getAttribute("usuario");
+        return u != null && !u.esAdministrativo();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ portal/src/main/webapp/presentation/cliente/vincularCuenta.jsp
@@ -0,0 +1,72 @@
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.Map"%>
+
+    <%@page contentType="text/html" pageEncoding="UTF-8"%>
+        <!DOCTYPE html>
+        <html>
+
+        <head>
+
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <title>Login</title>
+            <%@ include file="/presentation/Head.jsp" %>
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+
+        <body>
+             <jsp:include page="/presentation/Header.jsp" />
+            <% Usuario credenciales= (Usuario) request.getAttribute("credenciales"); %>
+            <%String  textoError = (String) request.getAttribute("textoError"); %>
+            <%Map<String, String>  errores = (Map<String, String>) request.getAttribute("errores"); %>
+            
+                <div class="contenido centrado">
+                    <div class="formulario">
+                        <h6>Iniciar sesion</h6>
+
+                        <form action="/portal/login" method="post">
+                            <%if(textoError!=null){%>
+                            <div class="erroneo">
+                                <label><%=textoError%></label>
+                                <%if(errores!=null){%>
+                                <%for(String error: errores.values()){%>
+                                    <label><%=error%></label>
+                                <%}}%>
+                            </div>
+                            <%}%>
+                            
+                            <div class="campo-entrada <%=erroneo("usuario",errores)%>">
+                                <input type="text" id="cedula" name="usuario" 
+                                       value="<%=credenciales.getCedula()==0? "" : credenciales.getCedula()%>"  
+                                       pattern="\d+" title="Ingrese una cedula valida sin guiones y/o espacios" placeholder=" " required>
+                                <label for="cedula">Cedula</label>
+                            </div>
+                            <div class="campo-entrada <%=erroneo("pass",errores)%>">
+                                <input type="password" id="contraseña" name="pass" value="<%=credenciales.getPass()%>" placeholder=" "  required>
+                                <label for="contraseña">Contraseña</label>
+                            </div>
+                            <div class="campo-entrada">
+                                <input type="checkbox" id="login-admin" value="login-admin" name="admin">
+                                <label for="login-admin">Login administrativo</label>
+                            </div>
+                            <button class="submit">Iniciar sesion</button>
+                        </form>
+
+                    </div>
+
+                </div>
+
+                <%@ include file="/presentation/Footer.jsp" %>
+        </body>
+
+        </html>
+        
+        
+ <%!
+    private String erroneo(String campo, Map<String,String> errores){
+      if ( (errores!=null) && (errores.get(campo)!=null) )
+        return "erroneo";
+      else
+        return "";
+    }
+   
+%> 
